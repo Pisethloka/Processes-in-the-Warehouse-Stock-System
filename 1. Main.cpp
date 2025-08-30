dad8599746d0ca3d246d9df4582dddbfe044a233
Pisethloka <Piseth.loka168@gmail.com>
2025-08-30 13:52:15 +0700

    Create 1. Main.cpp

---
+++ 1. Main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+using namespace std;
+
+void scoreMenu();
+void itemInfoMenu;
+
+void scoreMenu() {
+    int choice;
+    cout <<"1. Create Stock records" <<endl;
+    cout <<"2. Search Stock detail by ID: "<<endl;
+    cout <<"3. Update Stock for quantities (for incoming and outgoing stocks): "<<endl;
+    cout <<"4. Delete Stock record "<<endl;
+    cout <<"5. Exit"<<endl;
+    
+    cout <<"Enter your choice: "; cin>>choice;
+
+    switch (choice) {
+    case 1:
+        break;
+    
+    default:
+        break;
+    }
+}
