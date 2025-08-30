fb543f85d771a8f55d7bf8b7a241a2804fc9e175
Pisethloka <Piseth.loka168@gmail.com>
2025-08-30 14:51:38 +0700

    Update 1. Main.cpp
    Point 1 and 2 done stockMenu
    void itemInfoMenu functions are done!

--- 1. Main.cpp
+++ 1. Main.cpp
@@ -4,10 +4,20 @@
 #include<vector>
 using namespace std;
 
-void scoreMenu();
-void itemInfoMenu;
+void stockMenu();
+void itemInfoMenu();
 
-void scoreMenu() {
+void CreateStockRecord();
+void SearchStockDetails();
+void UpdateStockDetails();
+void DeleteStockRecord();
+
+void CreateItemInfo();
+void SearchItemInfo();
+void UpdateItemInfo();
+void DeleteItemInfo();
+
+void stockMenu() {
     int choice;
     cout <<"1. Create Stock records" <<endl;
     cout <<"2. Search Stock detail by ID: "<<endl;
@@ -19,12 +29,51 @@ void scoreMenu() {
 
     switch (choice) {
     case 1:
+        stockMenu;
         break;
+    case 2:
+        itemInfoMenu();
+        break;
+    case 3:
+        UpdateStockDetails();
+        break;
+    case 4:
+        DeleteStockRecord();
+        break;
+    case 5:
+        cout <<"Exiting the Program.";
+        break;
+
+    default: cout << "Invalid choice\n";
+    }
+}
+
+void itemInfoMenu() {
+    int choice;
+    cout <<"1. Create item information records."<<endl;
+    cout <<"2. Search item details by ID."<<endl;
+    cout <<"3. Update item details."<<endl;
+    cout <<"4. Delete item records."<<endl;
+    cout <<"5.Exit."<<endl;
+
+    switch (choice) {
+    case 1:
+        CreateItemInfo();
+        break;
+    case 2:
+        SearchItemInfo();
+        break;
+    case 3:
+        UpdateItemInfo();
+        break;
+    case 4:
+        DeleteItemInfo();
+        break;
+    case 5:
+        cout <<"Exiting the Program.";
+        break;
+    default: cout<<"invalid choice."<<endl;
     
-    default:
         break;
     }
-    cout << "Hello world";
-    cout << "Tra smos";
-    
 }
